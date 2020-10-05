82d0b46782f8c9831ae31821635ddc146e082b09
Wizzy-PC <wizzy83@gmail.com>
2020-10-05 19:21:24 +0200

    update

--- heart_failure_prediction.py
+++ heart_failure_prediction.py
@@ -1,8 +1,17 @@
+# imports
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 import pandas as pd
 
+# style options
 pd.set_option('display.width', 400)
 pd.set_option('display.max_columns', 30)
 sns.set_style('whitegrid')
+
+# load in the data
+df = pd.read_csv('heart_failure_clinical_records_dataset.csv', sep=',', header=0)
+
+# first look at the data
+df.head()
+df.describe()
