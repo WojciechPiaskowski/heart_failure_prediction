030d2b53c16d8d277d2700d84ab1a8337d72ecde
Wizzy-PC <wizzy83@gmail.com>
2020-10-05 19:17:29 +0200

    imports

---
+++ heart_failure_prediction.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import pandas as pd
+
+pd.set_option('display.width', 400)
+pd.set_option('display.max_columns', 30)
+sns.set_style('whitegrid')
